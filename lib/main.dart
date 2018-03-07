b34fb0e48e0891164f47412bf3fae309afce395e
ysreedhar <ya.sree@gmail.com>
2018-03-07 16:14:54 +0800

    Background initialize to red

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new Container(
-      color: Colors.redAccent,
+      color: Colors.red,
       child: new Center(
       child: new Text("Hello World", textDirection: TextDirection.ltr,)
       )
