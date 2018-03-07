f4122eb6a7db4c7ceea210d36bac0fdec9e9ad08
ysreedhar <ya.sree@gmail.com>
2018-03-07 16:14:00 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new Container(
-      color: Colors.red,
+      color: Colors.redAccent,
       child: new Center(
       child: new Text("Hello World", textDirection: TextDirection.ltr,)
       )
