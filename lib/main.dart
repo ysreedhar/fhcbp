fcd6d7c9f09efa37d64125e77be7503997a3cc37
ysreedhar <ya.sree@gmail.com>
2018-03-07 15:43:13 +0800

    Hello World Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Container(
+      color: Colors.blue,
+      child: new Center(
+      child: new Text("Hello World", textDirection: TextDirection.ltr,)
+      )
+  )
+  );
+}
