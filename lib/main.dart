4b00fc6805f5b2c0235b24692b4d0cc492b00d0a
ysreedhar <ya.sree@gmail.com>
2018-03-07 16:00:27 +0800

    Blue background changed to red

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new Container(
-      color: Colors.blue,
+      color: Colors.red,
       child: new Center(
       child: new Text("Hello World", textDirection: TextDirection.ltr,)
       )
